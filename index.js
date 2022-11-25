1498f14907da9a76e1f39cfe2afd9d6fef12340f
charlotte karimi <[charlotte.karimi@student.moringaschool.com]>
2022-11-25 09:19:34 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+
+let  companyName= 'Scuber'
+
+const companyName = 'Scuber'
+
+ let companyCeo = 'Susan Smith'
+ const companyCeo ='Susan Smith'
+
+let mostProfitableNeighborhood ='Princeville'
+
+const mostProfitableNeighborhood='Princevillle'
